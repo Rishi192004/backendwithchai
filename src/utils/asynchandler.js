93c35797fbd3452bdf6e020c5946bb1132f472f4
Rishi <rishib2482005@gmail.com>
2024-06-02 09:11:29 +0530

    nj

--- src/utils/asynchandler.js
+++ src/utils/asynchandler.js
@@ -8,3 +8,5 @@ const asynchandler=(fn)=> async(err,req,res,next)=>{
         })
     }
 }
+//abhi tk iska kaam smjh nhi aya,mtlb yeh to pta hai ki ye function krta kya hai but kyu use kiya hai
+export {asynchandler}
