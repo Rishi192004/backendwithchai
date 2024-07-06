5573f5cd9afbad761048f17f4f767d094bd79a55
Rishi <rishib2482005@gmail.com>
2024-07-06 21:37:53 +0530

    deubugging of my getSubscribedChannels

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(express.static("public"))
 
 //routes import
 import userRouter from './routes/user.router.js'
-import subscriptionRouter from "./routes/subscription.router.js"
+import subscriptionRouter from './routes/subscription.router.js'
 
 //routes declaration
 app.use("/api/v1/users",userRouter)
--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -5,6 +5,7 @@ import { APIError } from "../utils/APIError.js";
 import { asynchandler } from "../utils/asynchandler.js";
 
  const getSubscribedChannels=asynchandler(async(req,res)=>{
+    //console.log("hi hi hi hi hi")
     const userId=req.user?._id;
     if (!userId) {
         throw new APIError(400, "User not authenticated");
@@ -15,11 +16,13 @@ import { asynchandler } from "../utils/asynchandler.js";
     }
     //jaha subscriber pe meri id hogi mai un sare channel ka ek list bna raha hun
    const subscibedChannel=await subscription.find({subscriber:userId}).populate('channel','username fullname avatar')
+   console.log(subscibedChannel)
    if(subscibedChannel.length===0){
-    return new ApiResponse(200,{},"you have not subscribed any channel yet")
+    return res.status(200).json(new ApiResponse(200,{},"you have not subscribed any channel yet"))
    }
    return res
    .status(200)
    .json(new ApiResponse(200,subscibedChannel,"list of subscribed channels sent"))
  })
+
  export{ getSubscribedChannels }
