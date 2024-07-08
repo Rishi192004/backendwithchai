30506da13b8f354f028f61a210ca4317b8569905
Rishi <rishib2482005@gmail.com>
2024-07-08 18:01:56 +0530

    publish a video controller

--- src/controllers/videos.controller.js
+++ src/controllers/videos.controller.js
@@ -28,7 +28,80 @@ const addWatchedVideoInWatchHistory=asynchandler(async(req,res)=>{
     return res.status(200).json(new ApiResponse(200, {}, "Video added to watch history"));
 })
 
+const publishAVideo = asynchandler(async (req, res) => {
+    const { title, description} = req.body
+
+    
+    // TODO: get video, upload to cloudinary, create video
+
+
+    if(!title || ! description){
+        throw new APIError(400,"title or description is not entered")
+    }
+
+
+    const userId=req.user?._id;
+    if(!userId){
+        throw new APIError(400,"user not authenticated");
+    }
+
+
+    const user=await User.findById(userId);
+    if(!user){
+        throw new APIError(400,"user not found")
+    }
+
+
+    const videoFileLocalPath=req.files?.videoFile[0]?.path;
+    if(!videoFileLocalPath){
+        throw new APIError(400,"video file not uploaded on local server")
+    }
+
+
+    const thumbNailLocalPath=req.files?.thumbNail[0]?.path;
+    if(!thumbNailLocalPath){
+        throw new APIError(400,"thumb nail file not uploaded on local server")
+    }
+
+
+    const videoFile=await uploadOnCloudenary(videoFileLocalPath);
+    if(!videoFile){
+        throw new APIError(500,"video file local path not uploaded on cloudenary")
+    }
+
+
+    const thumbNail=await uploadOnCloudenary(thumbNailLocalPath);
+    if(!thumbNail){
+        throw new APIError(500,"thumbnail local path not uploaded on cloudenary")
+    }
+
+
+    if(!videoFile.duration){
+        throw new APIError(400,"duration is not defined in cloudenary response for video duration")
+    }
+
+
+    const video=await Video.create({
+        videoFile:videoFile?.url,
+        thumbNail:thumbNail?.url,
+        title:title,
+        description:description,
+        duration:videoFile.duration,
+        owner:userId
+    })
+
+
+    const videoConnect=await Video.findById(video._id).populate("owner","username")
+    if(!videoConnect){
+        throw new APIError(500,"something went wrong while uploading video")
+    }
+
+
+    return res.status(201).json(new ApiResponse(201,videoConnect,"video is succefully created and stored on database"));
+    
+})
 
 
 
-export { addWatchedVideoInWatchHistory }
+
+export { addWatchedVideoInWatchHistory, publishAVideo }
--- src/routes/video.router.js
+++ src/routes/video.router.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import {upload} from "../middleware/multer.middleware.js"
-import { addWatchedVideoInWatchHistory } from "../controllers/user.controller.js";
+import { addWatchedVideoInWatchHistory,publishAVideo} from "../controllers/user.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 const router=Router()
 
 
 
 router.route("/addToWatcheHistory").get(verifyJWT,addWatchedVideoInWatchHistory)
-
+router.route("/publishAVideo").post(upload.fields([{name:"videoFile",maxCount:1},{name:"thumbNail",maxCount:1}]),publishAVideo)
 
 
 
