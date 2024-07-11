06f987d219e45e7f410d24b0944f51b5c9b3cd80
Rishi <rishib2482005@gmail.com>
2024-07-11 17:25:56 +0530

    mm

--- src/routes/video.router.js
+++ src/routes/video.router.js
@@ -1,14 +1,19 @@
 import { Router } from "express";
 import {upload} from "../middleware/multer.middleware.js"
-import { addWatchedVideoInWatchHistory,publishAVideo,deleteVideo} from "../controllers/user.controller.js";
+import { addWatchedVideoInWatchHistory,publishAVideo,deleteVideo,updateVideo,getVideoById,toggleSubscription} from "../controllers/user.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 const router=Router()
 
 
 
 router.route("/addToWatcheHistory").get(verifyJWT,addWatchedVideoInWatchHistory)
-router.route("/publishAVideo").post(upload.fields([{name:"videoFile",maxCount:1},{name:"thumbNail",maxCount:1}]),publishAVideo)
+router.route("/publishAVideo").post(verifyJWT,upload.fields([{name:"videoFile",maxCount:1},{name:"thumbNail",maxCount:1}]),publishAVideo)
 router.route("/deleteVideo").post(verifyJWT,deleteVideo)
+router.route("/updateVideo").patch(verifyJWT,updateVideo)
+//I am not giving featue of updating thumbnail for now--->WILL CONSIDER LATER
+router.route("/getVideoById").get(verifyJWT,getVideoById)
+
+router.route("/toggle/publish/:videoId").patch(verifyJWT,toggleSubscription)
 
 
 
