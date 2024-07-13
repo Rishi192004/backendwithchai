8d6057327f4c4ceb9e5229035c36a54eb8ad253a
Rishi <rishib2482005@gmail.com>
2024-07-13 20:29:51 +0530

    playlist3.0

--- src/controllers/playList.controller.js
+++ src/controllers/playList.controller.js
@@ -2,30 +2,68 @@ import { asynchandler } from "../utils/asynchandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { APIError } from "../utils/APIError.js";
 import { playList } from "../models/playList.model.js";
+import { Video } from "../models/video.model.js";
 
 const createPlaylist = asynchandler(async (req, res) => {
-    const userId=req.user?._id;
-    if(!userId){
-        throw new APIError(400,"user is not authenticated")
+    const userId = req.user?._id;
+    if (!userId) {
+        throw new APIError(401, "User is not authenticated");
     }
 
+    const { name, description } = req.body;
     
-    const { name , description } = req.body
-    console.log(name);
-    console.log(description)
-    if(!name || !description){
-        throw new APIError(401,"name or description both are required")
+    if (!name || !description) {
+        throw new APIError(400, "Name and description are both required");
     }
      
-    const PlayList=await playList.create({
+    const playlist = await playList.create({
         name,
         description,
-        owner:userId
+        owner: userId
     });
 
-    if(!playList){
-        throw new APIError(500,"iternal server error")
+    if (!playlist) {
+        throw new APIError(500, "Internal server error");
     }
-    res.status(200).json(new ApiResponse(200,PlayList,"playList created"))
+
+    res.status(201).json(new ApiResponse(201, playlist, "Playlist created"));
+});
+
+const addVideoToPlaylist = asynchandler(async (req, res) => {
+    const {playlistId, videoId} = req.params
+    if(!playlistId || !videoId){
+        throw new APIError(400,"playlistId or videoId is missing")
+    }
+    const userId=req.user?._id;
+    if(!userId){
+        throw new APIError(400,"user not authenticated")
+    }
+    const video=await Video.findById(videoId);
+    if(!video){
+        throw new APIError(400,"no video found from videId")
+    }
+    const playlist=await playList.findByIdAndUpdate(playlistId,
+        {
+            $addToSet:{videos:videoId}
+        },
+        {new:true}
+    ) .populate({
+        path: 'videos',  // Path to populate
+        select: 'thumbNail owner videoFile title views',  // Fields to include from the Video model
+        populate: {  // Populate the owner field with username from the User model
+            path: 'owner',
+            select: 'username'
+        }
+    })
+    .populate({
+        path: 'owner',  // Populate the owner field of the Playlist
+        select: 'username'
+    });
+
+     
+    if(!playlist){
+        throw new APIError(400,"no playlist with playListId found")
+    }
+     return res.status(200).json(new ApiResponse(200,playlist,"added video to playList"))
 })
-export {createPlaylist}
+export {createPlaylist,addVideoToPlaylist}
--- src/routes/playList.router.js
+++ src/routes/playList.router.js
@@ -1,8 +1,8 @@
 import { Router } from "express"
 import { verifyJWT } from "../middleware/auth.middleware.js";
-import { createPlaylist } from "../controllers/playList.controller.js"
+import { createPlaylist,addVideoToPlaylist } from "../controllers/playList.controller.js"
 const router=Router()
 
 router.route("/createPlaylist").post(verifyJWT,createPlaylist)
-
+router.route("/addVideoToPlaylist/:playlistId/:videoId").post(verifyJWT,addVideoToPlaylist)
 export default router;
