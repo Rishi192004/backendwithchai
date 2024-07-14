4a9c953d25e275005d4115d08b9cd6af503cb9d1
Rishi <rishib2482005@gmail.com>
2024-07-14 20:38:37 +0530

    tweet model and other setting

---
+++ src/controllers/tweets.controller.js
@@ -0,0 +1,10 @@
+import { asynchandler } from "../utils/asynchandler.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { APIError } from "../utils/APIError.js";
+import { Tweet } from "../models/tweets.model.js";
+import { User } from "../models/user.model.js";
+
+
+
+
+export{}
---
+++ src/routes/tweets.router.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { verifyJWT } from '../middleware/auth.middleware.js';
+import {  } from '../controllers/tweets.controller.js'
+const router=Router();
+
+
+
+
+
+
+export default router;
